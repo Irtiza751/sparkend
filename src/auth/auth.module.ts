a46997b102e152888a424705cec89c0e8d6a681c
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2025-08-03 14:59:56 +0500

    add the google controller for social signin

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { GoogleController } from './social/google.controller';
 
 @Module({
-  controllers: [AuthController],
+  controllers: [AuthController, GoogleController],
   providers: [AuthService],
 })
 export class AuthModule {}
