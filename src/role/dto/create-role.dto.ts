36790e42d2645c6cc1ce6fdec7830f0f1203f1a4
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2025-08-03 21:32:33 +0500

    add role creation logic

--- src/role/dto/create-role.dto.ts
+++ src/role/dto/create-role.dto.ts
@@ -1,1 +1,11 @@
-export class CreateRoleDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString } from "class-validator";
+
+export class CreateRoleDto {
+  @IsString()
+  @ApiProperty({
+    description: 'The name of the role',
+    example: 'Admin',
+  })
+  name: string;
+}
--- src/role/role.service.ts
+++ src/role/role.service.ts
@@ -1,11 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
+import { InjectRepository } from '@mikro-orm/nestjs';
+import { Role } from './entities/role.entity';
+import { EntityManager, EntityRepository, UniqueConstraintViolationException } from '@mikro-orm/postgresql';
 
 @Injectable()
 export class RoleService {
-  create(createRoleDto: CreateRoleDto) {
-    return 'This action adds a new role';
+  constructor(
+    /**
+     * @description Injecting the User repository to perform CRUD operations
+     */
+    @InjectRepository(Role)
+    private readonly roleRepository: EntityRepository<Role>,
+    /**
+     * @description Injecting the EntityManager for advanced database operations
+     */
+    private readonly em: EntityManager,
+  ) { }
+
+  async create(createRoleDto: CreateRoleDto) {
+    try {
+      const role = this.roleRepository.create(createRoleDto);
+      await this.em.persistAndFlush(role);
+      return {
+        message: 'Role created successfully',
+        role,
+      };
+    } catch (error) {
+      if (error instanceof UniqueConstraintViolationException) {
+        throw new ConflictException(`Role with name '${createRoleDto.name}' already exists.`);
+      }
+      Logger.error('Error creating role:', error.message);
+      throw new InternalServerErrorException('Failed to create role');
+    }
   }
 
   findAll() {
