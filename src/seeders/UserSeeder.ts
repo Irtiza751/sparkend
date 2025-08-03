bcd05a09c1d0861d78d4883018a3d118ae939798
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2025-08-03 22:43:29 +0500

    setup the basic seeder

---
+++ src/seeders/UserSeeder.ts
@@ -0,0 +1,33 @@
+import type { Dictionary, EntityManager } from '@mikro-orm/core';
+import { Seeder } from '@mikro-orm/seeder';
+import { AuthProvider } from '../user/enums/auth-provider';
+import { User } from '../user/entities/user.entity';
+
+export class UserSeeder extends Seeder {
+
+  async run(em: EntityManager, context: Dictionary): Promise<void> {
+    const users = [
+      {
+        email: 'john.doe@example.com',
+        username: 'johndoe',
+        password: 'password123',
+        roles: [context.adminRole, context.userRole],
+        provider: AuthProvider.LOCAL,
+        isEmailVerified: true,
+      },
+      {
+        email: 'jane.doe@example.com',
+        username: 'janedoe',
+        password: 'password123',
+        roles: [context.adminRole, context.userRole],
+        provider: AuthProvider.LOCAL,
+        isEmailVerified: false,
+      },
+    ]
+
+    for (let userData of users) {
+      em.create(User, userData)
+    }
+  }
+
+}
