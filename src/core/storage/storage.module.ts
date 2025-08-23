d30f98d935c35cfbcc08890dfa65f1804fdc334f
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2025-08-23 15:00:43 +0500

    Add support for feature-based storage providers with async configuration

--- src/core/storage/storage.module.ts
+++ src/core/storage/storage.module.ts
@@ -5,9 +5,16 @@ import { StorageService } from './storage.service';
 import storageConfig from './config/storage.config';
 import { StorageType } from './enums/storage-type';
 import { STORAGE_PROVIDER } from './constants/storage';
+import { StorageProvider } from './interfaces/storage-provider';
 
 interface StorageModuleOptions {
   type: StorageType;
+}
+
+export interface StorageFeatureOptions {
+  name: string;
+  type?: StorageType;
+  config?: Record<string, any>;
 }
 
 @Module({
@@ -53,6 +60,74 @@ export class StorageModule {
       ],
       exports: [StorageService],
       global: true,
+    };
+  }
+
+  static forFeature(options: StorageFeatureOptions): DynamicModule {
+    const featureProviderToken = `STORAGE_PROVIDER_${options.name.toUpperCase()}`;
+    const featureServiceToken = `STORAGE_SERVICE_${options.name.toUpperCase()}`;
+
+    const providers = [
+      {
+        provide: featureProviderToken,
+        useFactory: () => {
+          const ProviderClass = this.getStorageProvider(
+            options.type || StorageType.DISK,
+          );
+          return new ProviderClass(options.config);
+        },
+      },
+      {
+        provide: featureServiceToken,
+        useFactory: (provider: StorageProvider) => {
+          return new StorageService(provider);
+        },
+        inject: [featureProviderToken],
+      },
+    ];
+
+    return {
+      module: StorageModule,
+      providers,
+      exports: [featureServiceToken],
+    };
+  }
+
+  static forFeatureAsync(options: {
+    name: string;
+    useFactory: (
+      ...args: any[]
+    ) => StorageFeatureOptions | Promise<StorageFeatureOptions>;
+    inject?: any[];
+  }): DynamicModule {
+    const featureProviderToken = `STORAGE_PROVIDER_${options.name.toUpperCase()}`;
+    const featureServiceToken = `STORAGE_SERVICE_${options.name.toUpperCase()}`;
+
+    const providers = [
+      {
+        provide: featureProviderToken,
+        useFactory: async (...args: any[]) => {
+          const config = await options.useFactory(...args);
+          const ProviderClass = this.getStorageProvider(
+            config.type || StorageType.DISK,
+          );
+          return new ProviderClass(config.config);
+        },
+        inject: options.inject || [],
+      },
+      {
+        provide: featureServiceToken,
+        useFactory: (provider: StorageProvider) => {
+          return new StorageService(provider);
+        },
+        inject: [featureProviderToken],
+      },
+    ];
+
+    return {
+      module: StorageModule,
+      providers,
+      exports: [featureServiceToken],
     };
   }
 
